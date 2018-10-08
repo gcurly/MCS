4782614941b54d36eb6e5203ca0ed1dc09ca2356
gcurly <gcurly@mail.ru>
2018-10-09 00:04:26 +0300

    module.js changed

--- boilerplate/js/module.js
+++ boilerplate/js/module.js
@@ -1,7 +1,8 @@
 let Num = parseInt(prompt('Enter number'));
-if (Num < 0){
-	console.log(-Num);
-} 
-else {
-	console.log(Num);
-}
+// if (Num < 0){
+// 	console.log(-Num);
+// } 
+// else {
+// 	console.log(Num);
+// }
+Num > 0 ? console.log(Num):console.log(-Num);
